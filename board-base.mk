305bf9fa12f0d859dc3e318d46a317a8e4bbde8f
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-27 14:51:22 +0000

    Chmod 775 graphic composer

--- board-base.mk
+++ board-base.mk
@@ -1,3 +1,4 @@
+TARGET_FS_CONFIG_GEN += device/phh/treble/huawei_hwc/huawei_hwc.fs
 TARGET_SYSTEM_PROP := device/phh/treble/system.prop $(TARGET_SYSTEM_PROP)
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/phh/treble/bluetooth
 TARGET_EXFAT_DRIVER := exfat
