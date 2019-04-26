f26c621ee85ad280fa05d00faa3772171d80e38e
RealJohnGalt <johngalt@fake.mail>
2019-04-25 18:33:46 -0700

    initial motoactions

--- ali.mk
+++ ali.mk
@@ -2,7 +2,6 @@ $(call inherit-product, device/phh/treble/base-pre.mk)
 include build/make/target/product/treble_common.mk
 $(call inherit-product, vendor/vndk/vndk-binder32.mk)
 $(call inherit-product, device/phh/treble/base.mk)
-$(call inherit-product, device/phh/treble/gapps.mk)
 $(call inherit-product, vendor/motorola/ali/ali-vendor.mk)
 $(call inherit-product, device/phh/treble/descendant.mk)
 $(call inherit-product, device/qcom/sepolicy/sepolicy.mk)
@@ -26,6 +25,7 @@ BUILD_FINGERPRINT := motorola/ali/ali:9/PPS29.55-24/a37fd:user/release-keys
 PRODUCT_PACKAGES += \
     ims-ext-common \
     telephony-ext \
+    MotoActions \
     Stk
 
 PRODUCT_BOOT_JARS += \
