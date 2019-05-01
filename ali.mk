4fddac4169feba15b552ced03db53757511ec9b4
RealJohnGalt <johngalt@fake.mail>
2019-05-01 13:48:20 -0700

    ali.mk:add back gapps inheritance

--- ali.mk
+++ ali.mk
@@ -2,6 +2,7 @@ $(call inherit-product, device/phh/treble/base-pre.mk)
 include build/make/target/product/treble_common.mk
 $(call inherit-product, vendor/vndk/vndk-binder32.mk)
 $(call inherit-product, device/phh/treble/base.mk)
+$(call inherit-product, device/phh/treble/gapps.mk)
 $(call inherit-product, vendor/motorola/ali/ali-vendor.mk)
 $(call inherit-product, device/phh/treble/descendant.mk)
 $(call inherit-product, device/qcom/sepolicy/sepolicy.mk)
